1d24e76ff51a69b74e0e183038a39c7c6399341c
Healex04 <healex04@gmail.com>
2024-05-31 14:22:49 +0200

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,7 @@
+function showDropdown() {
+    document.getElementById("dropdownContent").style.display = "block";
+}
+
+function hideDropdown() {
+    document.getElementById("dropdownContent").style.display = "none";
+}
